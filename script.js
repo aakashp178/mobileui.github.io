0295afaf229bae6d8e57866037cbe8ef3c2de4d3
AakashMyrsa <aakash@myrsa.co>
2021-12-19 15:36:06 +0530

    initail commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // Toggle menu on click
+    // $("#menu-toggler").click(function () {
+        // document.body.classList.toggle('menu-active');
+        // document.getElementById("myDIV").classList.toggle("newClassName");
+    // });
+});
+
+function myFunction() {
+    document.body.classList.toggle("menu-active");
+}
